d9cbbc21a2b4265862d8e6f0de1501f0ccd34df5
Paul Melis <paul.melis@surfsara.nl>
2018-02-23 22:55:44 +0100

    Cleanups

--- progressbar.py
+++ progressbar.py
@@ -1,10 +1,33 @@
 #!/usr/bin/env python3
-import sys, time
+#
+# Copyright (c) 2018 Paul Melis
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# 
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+from sys import stdout
+from time import time
 
 class ProgressBar:
     
-    ROTOR = ['-', '\\', '|', '/']
-
     def __init__(self, prefix=None, max_value=None, interval=0.5):
         self.max_value = max_value
         self.prefix = ''
@@ -15,43 +38,44 @@ class ProgressBar:
         self.current_value = 0
         self.last_msg = ''
         self.rotor_index = 0
-        self.last_print = time.time()
-        self.t0 = time.time()
+        self.last_print = time()
+        self.t0 = time()
         
     def update(self, value):
         self.current_value = value
-        if time.time() - self.last_print > self.interval:
+        if time() - self.last_print > self.interval:
             self.rotor_index = (self.rotor_index+1) % 4
-            rotor = self.ROTOR[self.rotor_index]
+            rotor = ['-', '\\', '|', '/'][self.rotor_index]
             msg = '{0} {1} {2:,}'.format(self.prefix, rotor, value)
             self._print(msg)
         
     def finish(self):
-        self.t1 = time.time()
+        self.t1 = time()
         tdiff = self.t1 - self.t0
         msg = '{0} ... {1:,} ({2:.3f}s)'.format(self.prefix, self.current_value, tdiff)
         self._print(msg)
-        sys.stdout.write('\n')
+        stdout.write('\n')
         
     def _print(self, msg):
         lm = len(self.last_msg)
         wipe = lm * '\r'
-        sys.stdout.write(wipe)
-        sys.stdout.write(msg)
+        stdout.write(wipe)
+        stdout.write(msg)
         ns = lm - len(msg)
         if ns > 0:
-            sys.stdout.write(' '*ns)
-        sys.stdout.flush()
+            stdout.write(' '*ns)
+        stdout.flush()
         self.last_msg = msg
-        self.last_print = time.time()
+        self.last_print = time()
     
     
 if __name__ == '__main__':
     
+    from time import sleep
     b = ProgressBar('Doing something', interval=0.1)
     
     for i in range(300):
-        time.sleep(0.01)
+        sleep(0.01)
         b.update(300-i)
         
     b.finish()
